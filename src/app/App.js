76a52b327952a02057efce5cc5b3aea8fd5b90c5
Doomer77 <newzibkoff1984@gmail.com>
2022-04-01 14:24:15 +0300

    header component completed

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,16 @@
+import React from 'react'
+import Header from '../components/header'
 import Navbar from '../components/navbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
-    return <Navbar />
+    return (
+        <>
+            <Navbar />
+            <Header />
+        </>
+    )
 }
 
 export default App
