dcdf00a69e315db060b083d6843cbe0f02861ac9
Doomer77 <newzibkoff1984@gmail.com>
2022-04-04 15:59:42 +0300

    contacts component completed styling, add send email, addeded validation

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,31 +1,55 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
+import { useForm } from 'react-hook-form'
 import './Contacts.css'
 
 const Contacts = () => {
+    const form = useRef()
+    const [successMessage, setSuccessMessage] = useState('')
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
     const serviceID = 'service_ID'
     const templateID = 'template_ID'
-    const userId = 'J1xCpajP3l9r-Wf_6'
+    const userID = 'J1xCpajP3l9r-Wf_6'
 
-    const form = useRef()
-
-    const sendEmail = e => {
-        e.preventDefault()
-
-        emailjs.sendForm(serviceID, templateID, form.current, userId).then(
-            result => {
-                console.log(result.text)
+    const onSubmit = (data, r) => {
+        sendEmail(
+            serviceID,
+            templateID,
+            {
+                name: data.name,
+                phone: data.phone,
+                email: data.email,
+                subject: data.subject,
+                description: data.description,
             },
-            error => {
-                console.log(error.text)
-            }
+            userID
         )
+        r.target.reset()
     }
+
+    const sendEmail = (serviceID, templateID, variables, userID) => {
+        emailjs
+            .send(serviceID, templateID, variables, userID)
+            .then(() => {
+                setSuccessMessage(
+                    'Спасибо за оформления заказа, мы обязательно с Вами свяжемся'
+                )
+            })
+            .catch(err => console.error(`Something went wrong ${err}`))
+    }
+
     return (
         <section className="contacts-wrapper">
             <div className="contacts-heading">Оформить заказ:</div>
             <div className="container">
-                <form ref={form} onSubmit={sendEmail}>
+                <span className="success-message">{successMessage}</span>
+
+                <form ref={form} onSubmit={handleSubmit(onSubmit)}>
                     <div className="row">
                         <div className="col-md-6 col-xs-12">
                             <input
@@ -34,28 +58,65 @@ const Contacts = () => {
                                 className="form-control"
                                 placeholder="Ваше имя:"
                                 name="name"
+                                {...register('name', {
+                                    required:
+                                        'Пожалуйста, введите корректное имя',
+                                    maxLength: {
+                                        value: 20,
+                                        message:
+                                            'Пожалуйста, введите не более 20 символов',
+                                    },
+                                })}
                             />
+                            <span className="error-message">
+                                {errors.name && errors.name.message}
+                            </span>
+
                             <input
                                 id="phone"
                                 type="phone"
                                 className="form-control"
                                 placeholder="Телефон:"
                                 name="phone"
+                                {...register('phone', {
+                                    required:
+                                        'Пожалуйста, введите корректный телефон',
+                                })}
                             />
+                            <span className="error-message">
+                                {errors.phone && errors.phone.message}
+                            </span>
                             <input
                                 id="email"
                                 type="email"
                                 className="form-control"
                                 placeholder="Почта:"
                                 name="email"
+                                {...register('email', {
+                                    required:
+                                        'Пожалуйста, введите корректную почту',
+                                    pattern: {
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                        message: 'Не корректная почта',
+                                    },
+                                })}
                             />
+                            <span className="error-message">
+                                {errors.email && errors.email.message}
+                            </span>
                             <input
                                 id="subject"
                                 type="text"
                                 className="form-control"
                                 placeholder="Объект:"
                                 name="subject"
+                                {...register('subject', {
+                                    required: 'Пожалуйста, введите тип объекта',
+                                })}
                             />
+                            <span className="error-message">
+                                {errors.subject && errors.subject.message}
+                            </span>
                         </div>
                         <div className="col-md-6 col-xs-12">
                             <textarea
@@ -64,7 +125,15 @@ const Contacts = () => {
                                 placeholder="Проводимые работы:"
                                 className="form-control"
                                 name="description"
+                                {...register('description', {
+                                    required:
+                                        'Введите предпологаемый перечень работ',
+                                })}
                             ></textarea>
+                            <span className="error-message">
+                                {errors.description &&
+                                    errors.description.message}
+                            </span>
                             <button
                                 className="btn-main-offer contact-btn"
                                 type="submit"
